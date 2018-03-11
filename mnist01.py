312a3d109685e69ecfb3a159240bb2a826e074f9
gichanlee <gichan.lee@3mindev.clubgit init Mytestexit()>
2018-03-12 06:56:34 +0900

    add space after #

--- mnist01.py
+++ mnist01.py
@@ -5,17 +5,17 @@ Gets to 99.25% test accuracy after 12 epochs
 16 seconds per epoch on a GRID K520 GPU.
 '''
 
-#function to use of python 3 from use of python 2
+# function to use of python 3 from use of python 2
 from __future__ import print_function
 
-#use keras library
+# use keras library
 import keras
 
-#import module function 'mnist' from module name 'keras.datasets'
+# import module function 'mnist' from module name 'keras.datasets'
 from keras.datasets import mnist
 
-#sequence : a particular order in which related things follow each other
-#import sequntial model from keras.models
+# sequence : a particular order in which related things follow each other
+# import sequntial model from keras.models
 from keras.models import Sequential
 
 '''
@@ -28,17 +28,18 @@ flatten in deep learning : the process of converting all the resultant 2 demensi
 '''
 from keras.layers import Dense, Dropout, Flatten
 
-#Conv2D : constructs a two-dimensional "convolutional" layer
-#MaxPooling2D : constructs a two-dimentional 'pooling' layer
+# Conv2D : constructs a two-dimensional "convolutional" layer
+# MaxPooling2D : constructs a two-dimentional 'pooling' layer
 from keras.layers import Conv2D, MaxPooling2D
 
-#backend module : one of TensorFlow, Theano, CNTK
+# backend module : one of TensorFlow, Theano, CNTK
 from keras import backend as K
 
-#batch_size : number of test samples
+# batch_size : number of test samples
 batch_size = 128
-#num_classes : not sure
+# num_classes : not sure
 num_classes = 10
-#epochs : one pass of the full training set
+# epochs : one pass of the full training set
 epochs = 2
 
+#
